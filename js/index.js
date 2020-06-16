1bd947bedd5e34caa4a097523ef20f7de08209e3
Sora <sora78700@gmail.com>
2020-06-16 13:40:09 +0200

    multiples ajouts
    Ajout du bg, ajutements couleurs,  ajout du menu déroulant...

--- js/index.js
+++ js/index.js
@@ -1,6 +1,28 @@
-//Sticky menu
-$(document).ready(function() {
-    $('.menu').click(function() {
-        $('ul').toggleClass('active');
-    })
-})
+//modal trigger
+var trigger = document.querySelector(".trigger");
+var trigger2 = document.querySelector(".trigger2");
+var trigger3 = document.querySelector(".trigger3");
+var trigger4 = document.querySelector(".trigger4");
+var trigger5 = document.querySelector(".trigger5");
+var trigger6 = document.querySelector(".trigger6");
+var modal = document.querySelector(".modal");
+var closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+trigger2.addEventListener("click", toggleModal);
+trigger3.addEventListener("click", toggleModal);
+trigger4.addEventListener("click", toggleModal);
+trigger5.addEventListener("click", toggleModal);
+trigger6.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
